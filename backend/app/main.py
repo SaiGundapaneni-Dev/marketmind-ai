4971030fa85fb8283b82697287d775f42bdfdfd1
SaiGundapaneni-Dev <212675472+SaiGundapaneni-Dev@users.noreply.github.com>
2026-07-06 14:47:08 -0500

    Sprint 4 - Add application logging

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from app.api.portfolio import router as portfolio_router
 from app.core.config import settings
+from app.core.logger import setup_logger
+
+logger = setup_logger()
 
 app = FastAPI(
     title=settings.app_name,
@@ -12,6 +15,8 @@ app.include_router(portfolio_router)
 
 @app.get("/")
 def home():
+    logger.info("Root endpoint called")
+
     return {
         "message": f"Welcome to {settings.app_name} 🚀",
         "environment": settings.environment
